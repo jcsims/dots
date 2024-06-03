a5495611a3ece85bb8892ea0f7f2820c9c2b5833
Chris Sims <chris@jcsi.ms>
2024-06-02 21:41:54 -0700

    Make sure mkalias is available and emacs-plus aliased

--- bootstrap.d/mac.fish
+++ bootstrap.d/mac.fish
@@ -19,3 +19,13 @@ end
 info "Set key repeat rate and delay..."
 defaults write -g InitialKeyRepeat -int 15
 defaults write -f KeyRepeat -int 2
+
+if ! test -e $HOME/bin/mkalias
+    info "Grabbing mkalias..."
+    curl 'https://f000.backblazeb2.com/file/mkalias/mkalias' -o $HOME/bin/mkalias; and chmod +x $HOME/bin/mkalias
+end
+
+if ! test -e /Applications/Emacs.app
+    info "Creating alias for emacs-plus..."
+    mkalias "$(brew --prefix emacs-plus)/Emacs.app" /Applications/Emacs.app
+end
