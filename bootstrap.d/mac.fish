6bd46a3318a222519233b1679ee4b44fd7192363
Chris Sims <chris@jcsi.ms>
2024-06-18 15:45:32 -0700

    bootstrap: Remove an used comment

--- bootstrap.d/mac.fish
+++ bootstrap.d/mac.fish
@@ -4,9 +4,6 @@ if ! test -e $target_jdk_path; or ! test /opt/homebrew/opt/openjdk/libexec/openj
     sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk $target_jdk_path
 end
 
-# TODO: Compile jinx module on macos?
-# gcc -I. -O2 -Wall -Wextra -fPIC -shared -o jinx-mod.dylib jinx-mod.c -I/opt/homebrew/opt/enchant/include/enchant-2 -L /opt/homebrew/opt/enchant/lib -lenchant-2
-
 info "Set key repeat rate and delay..."
 defaults write -g InitialKeyRepeat -int 15
 defaults write -f KeyRepeat -int 2
