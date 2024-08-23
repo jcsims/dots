41fe241caecbf1cf6ec9651b46ff6fc6f7d779ee
Chris Sims <chris@jcsi.ms>
2024-08-22 21:17:16 -0700

    macOS: Add some more defaults configuration

--- bootstrap.d/mac.fish
+++ bootstrap.d/mac.fish
@@ -4,7 +4,8 @@ if ! test -e $target_jdk_path; or ! test /opt/homebrew/opt/openjdk/libexec/openj
     sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk $target_jdk_path
 end
 
-info "Set key repeat rate and delay..."
+info "Configuring keyboard"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 defaults write -g InitialKeyRepeat -int 15
 defaults write -f KeyRepeat -int 2
 
@@ -13,6 +14,20 @@ defaults write -g com.apple.swipescrolldirection -boolean NO
 
 info "Turning off strange bold fonts in Alacritty"
 defaults write org.alacritty AppleFontSmoothing -int 0
+
+info "Configuring the Dock"
+defaults write com.apple.dock orientation -string left
+defaults write com.apple.dock tilesize -int 40
+# Don't show recent apps in the Dock
+defaults write com.apple.dock show-recents -bool false
+# This only shows open applications, which is awesome
+defaults write com.apple.dock static-only -bool true
+# This makes hidden apps slightly darker
+defaults write com.apple.dock showhidden -bool true
+killall Dock
+
+info "Configuring Finder"
+defaults write com.apple.finder ShowPathbar -bool true
 
 if ! test -e $HOME/bin/mkalias
     info "Grabbing mkalias..."
