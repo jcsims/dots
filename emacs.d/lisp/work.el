16b24285f4c425537920a6127ac467ad8b498112
Chris Sims <chris@jcsi.ms>
2025-06-12 19:06:27 -0500

    Revert "Remove Obsidian package"
    This reverts commit 7fe73217b2c330ee57856468187e403ded4f1682.

--- emacs.d/lisp/work.el
+++ emacs.d/lisp/work.el
@@ -61,6 +61,7 @@
   ;; Interpret ANSI color codes in compilation buffer
   :hook (compilation-filter . ansi-color-compilation-filter))
 
+
 (use-package gptel
   :config
   (gptel-make-ollama "Ollama"
@@ -68,6 +69,30 @@
     :stream t
     :models '(devstral:latest))
   (setq gptel-model 'devstral:latest))
+
+(use-package obsidian
+  :demand t
+  :custom (obsidian-directory "~/notes/notes")
+  :config
+  (global-obsidian-mode)
+
+  (require 'dash)
+  (require 's)
+  (require 'seq)
+  (defun jcs/open-todays-meeting ()
+    "Open an Obsidian meeting note from today."
+    (interactive)
+    (let* ((today-string (format-time-string "%Y-%m-%d"))
+           (meeting-dir (expand-file-name "splash/meetings" obsidian-directory))
+           (choices (->> (directory-files-recursively meeting-dir "\.*.md$")
+                         (seq-filter #'obsidian-file-p)
+                         (seq-map (lambda (f) (file-relative-name f meeting-dir)))
+                         (seq-filter (lambda (f) (s-starts-with? today-string f))))))
+      (if choices
+          (obsidian-find-file (expand-file-name (completing-read "Select file: " choices)
+                                                meeting-dir))
+        (message "No meeting files for today.")))))
+
 
 (provide 'work)
 ;;; work.el ends here
