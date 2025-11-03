bfdea69c875a6b2ef184befb2281fd91c02bad8f
Chris Sims <chris@jcsi.ms>
2025-11-03 11:16:02 -0800

    Move to a simpler bookmark launcher

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -968,7 +968,7 @@ Passes ARG onto `zap-to-char` or `backward-kill-word` if used."
   (global-set-key (kbd "C-c e f") (lambda () (interactive) (find-file "~/.config/fish/config.fish")))
   (global-set-key (kbd "C-c e p") (lambda () (interactive) (find-file "~/.Brewfile")))
   (global-set-key (kbd "C-c e w") (lambda () (interactive) (find-file "~/.emacs.d/lisp/work.el")))
-  (global-set-key (kbd "C-c e b") (lambda () (interactive) (find-file "~/.emacs.d/work-bookmarks/work-bookmarks.el")))
+  (global-set-key (kbd "C-c e b") (lambda () (interactive) (find-file "~/.bookmarks")))
   (global-set-key (kbd "C-c e j") (lambda () (interactive) (find-file "~/Library/Mobile Documents/iCloud~com~xenodium~Journelly/Documents/Journelly.org")))
 
   ;; Taken from the Emacs Wiki: http://www.emacswiki.org/emacs/InsertDate
@@ -1136,11 +1136,6 @@ format. With PREFIX, copy to kill ring."
 
   (load-file-if-present (concat (user-real-login-name) ".el"))
   (load-file-if-present "lisp/chatgpt-shell.el")
-
-  ;; Bookmark helper
-  (load-file-if-present "work-bookmarks/work-bookmarks.el")
-  (load-file-if-present "lisp/personal-bookmarks.el")
-  (load-file-if-present "lisp/bookmark-frame.el")
 
   ;; Work config
   (when work-install
