bb3d59254f25a6fb9997649aaa196063c794519a
Chris Sims <chris@jcsi.ms>
2025-12-16 17:33:32 -0800

    Revert "Move to a simpler bookmark launcher"
    This reverts commit bfdea69c875a6b2ef184befb2281fd91c02bad8f.

---
+++ emacs.d/lisp/personal-bookmarks.el
@@ -0,0 +1,13 @@
+;;; package --- Summary
+
+;;; Commentary:
+;; Bookmarks for personal things.
+
+;;; Code:
+
+(defvar personal-bookmarks
+  '())
+
+
+(provide 'personal-bookmarks)
+;;; personal-bookmarks.el ends here
